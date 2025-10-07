939e092a352ed12bacbce2d31b2617fa6b417c49
Ayush Sinha <135677365+Proayush22@users.noreply.github.com>
2025-10-07 01:08:25 -0400

    Add files via upload

---
+++ Portfolio/index.js
@@ -0,0 +1,5 @@
+//ScrollReveal().reveal('.sidebar', { origin: 'left', distance: '200px', duration: 1000 });
+ScrollReveal().reveal('.navbar', { origin: 'top', distance: '50px', duration: 750, reset: true });
+ScrollReveal().reveal('.clients-item', { easing: 'ease-out', interval: 200 , reset: true });
+ScrollReveal().reveal('.skills-progress-bg', { origin:left, distance:"50px",interval:100,duration:1000, reset: true });
+//ScrollReveal().reveal('.social-item', { rotate: { x: 90, y: 90, z: 90 }, interval: 200, reset: true });
